96a5d287c0618837d93185b5e28efdb698ddf0b3
Minhkunn <minh.12072k6@gmail.com>
2026-03-04 21:05:24 +0700

    (feat): define project environment variables and apply Singleton Pattern

---
+++ src/config.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+from typing import List
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "ShortestPath"
+    DATA_PATH: str = "data"
+    PLACE_NAME: str = "Hoan Kiem, Hanoi, Vietnam"
+
+    # Development variables
+    APP_ENV: str = "development"
+    DEBUG: bool = True
+
+    # --- Server Configuration ---
+    HOST: str = "0.0.0.0"
+    PORT: int = 8080
+    ALLOWED_HOSTS: List[str] = ["localhost", "127.0.0.1", "0.0.0.0"]
+    ALLOWED_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8080"]
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
