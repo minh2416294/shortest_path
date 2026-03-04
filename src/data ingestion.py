b540052250bef172e9856db5eca724613f64b6ea
Minhkunn <minh.12072k6@gmail.com>
2026-03-04 21:13:39 +0700

    (feat): download and preprocess map data

---
+++ src/data ingestion.py
@@ -0,0 +1,47 @@
+import osmnx as ox
+import json
+import os
+from src.config import get_settings
+
+settings = get_settings()
+
+
+def download_and_preprocess_map(place_name, output_dir):
+    print(f"Downloading map for {place_name}...")
+    G = ox.graph_from_place(place_name, network_type='drive') # Only install drive path
+    print(f"Finished downloading! Graph has {len(G.nodes)} nodes and {len(G.edges)} edges.")
+
+    nodes_data = {}
+    edges_data = []
+
+    print("Preprocessing map...")
+    # Get latitude and longitude
+    for node_id, data in G.nodes(data=True):
+        nodes_data[node_id] = {
+            "y": data['y'],  # get latitude
+            "x": data['x']  # get longitude
+        }
+
+    # Get edges and path length
+    for u, v, key, data in G.edges(keys=True, data=True):
+        edges_data.append({
+            "u": u,
+            "v": v,
+            "length": data.get('length', 1.0)  # Fallback is 1.0 meter
+        })
+
+    # Save nodes and edges files
+    os.makedirs(output_dir, exist_ok=True)
+    nodes_path = os.path.join(output_dir, "nodes.json")
+    edges_path = os.path.join(output_dir, "edges.json")
+
+    with open(nodes_path, "w", encoding="utf-8") as f:
+        json.dump(nodes_data, f)
+    with open(edges_path, "w", encoding="utf-8") as f:
+        json.dump(edges_data, f)
+
+    print(f"Saved: {nodes_path} and {edges_path}")
+
+
+if __name__ == "__main__":
+    download_and_preprocess_map(settings.PLACE_NAME, settings.DATA_PATH)
